3397745be06d26d5cebfb49922d00bc4ba1d0842
Gurram Karthik <167804249+gurramkarthiknetha@users.noreply.github.com>
2025-03-29 16:31:23 +0530

    fixed issue

--- feedbackapp/src/components/RootLayout.jsx
+++ feedbackapp/src/components/RootLayout.jsx
@@ -4,18 +4,6 @@ import { Outlet } from 'react-router-dom';
 
 function RootLayout() {
   return (
-<<<<<<< HEAD
-    <div style={{ display: 'flex', height: '100vh'}}>
-      {/* Sidebar with scrolling */}
-      <div style={{ width: '350px', overflowY: 'auto' }}>
-        <SideBar />
-      </div>
-
-      {/* Main content area */}
-      <div style={{ flexGrow: 1, overflowY: 'auto'}}>
-        <Outlet />
-      </div>
-=======
     <div className='d-flex'>
         <div className='' style={{width:'300px'}} >
           <SideBar/>
@@ -23,7 +11,6 @@ function RootLayout() {
         <div className='flex-grow-1'>
           <Outlet/>
         </div>
->>>>>>> 318c7cbe5b2521f0c8bbb0f2227d9bbea8eded12
     </div>
   );
 }
